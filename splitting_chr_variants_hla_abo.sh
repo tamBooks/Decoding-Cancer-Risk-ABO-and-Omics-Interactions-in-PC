9e491b76e0f2a9a2ae1c4338c00583c630fcc2a1
tamara <117675924+tamBooks@users.noreply.github.com>
2025-01-06 19:06:20 +0100

    Add files via upload

---
+++ splitting_chr_variants_hla_abo.sh
@@ -0,0 +1,11 @@
+# HLA extraction
+zcat chr6.dose.vcf.gz | grep "^#"  > HLA_region.vcf
+zcat chr6.dose.vcf.gz | awk '{FS="\t"} {if ($2>=27970031 && $2<=33965553) {print $0}}' >> HLA_region.vcf
+
+zcat chr6.info.gz | grep "^#"  > HLA_region.info
+zcat chr6.info.gz | awk '{FS="\t"} {if ($2>=27970031 && $2<=33965553) {print $0}}' >> HLA_region.info
+
+# ABO extraction
+zcat chr9.dose.vcf.gz | grep "^#"  > abo_region.vcf
+zcat chr9.dose.vcf.gz | awk '{FS="\t"} {if ($2 >= 133250000 && $2 <= 133276000) {print $0}}' >> abo_region.vcf
+#awk '{FS="\t"} {if ($2>=136115788 && $2<=136160617) {print $0}}' >> abo_region.vcf
