e3a6433b2bbcec69fbe79fe271fdaf5605f70a7f
tamara <117675924+tamBooks@users.noreply.github.com>
2025-01-07 12:03:19 +0100

    Update dataprep2019.sh

--- dataprep2019.sh
+++ dataprep2019.sh
@@ -1,6 +1,6 @@
 # Data Preperation for imputation
 # Run perl script that is set by the Imputatipn server
-perl /local/tsaid/trabajo/dataprep/HRC-1000G-check-bim-v4.3.0/HRC-1000G-check-bim.pl -b /local/tsaid/trabajo/vcf/merged2024/merged_panGen2024.bim -f /local/tsaid/trabajo/vcf/merged2024/merged_panGen2024.afreq -r /local/tsaid/trabajo/dataprep/HRC-1000G-check-bim-v4.3.0/HRC.r1-1.GRCh37.wgs.mac5.sites.tab -h sh Run-plink.sh
+perl /local/tsaid/trabajo/dataprep/HRC-1000G-check-bim-v4.3.0/HRC-1000G-check-bim.pl -b /local/tsaid/trabajo/vcf/merged2024/merged_panGen2024.bim -f /local/tsaid/trabajo/vcf/merged2024/merged_panGen2024.afreq -r /local/tsaid/trabajo/dataprep/HRC-1000G-check-bim-v4.3.0/HRC.r1-1.GRCh37.wgs.mac5.sites.tab -g sh Run-plink.sh
 
 # Edit and run the plink script made by the perl script
 bash Run-plink.sh
